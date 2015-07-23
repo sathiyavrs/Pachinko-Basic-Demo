91269da4d66c1958d383379a498e1d44a6355819
sathiyavrs <sathiyavrs@gmail.com>
2015-07-23 07:06:37 +0530

    Version 0.2

--- Assets/Code/LaunchControl.cs
+++ Assets/Code/LaunchControl.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class LaunchControl : MonoBehaviour
 {
@@ -6,6 +7,10 @@ public class LaunchControl : MonoBehaviour
 
     private Rigidbody2D _rigidBody;
     private bool _hasLaunched;
+
+    // Hardcoding
+    private List<Vector2> EvilThrusts;
+    private List<Vector2> PrizeworthyThrusts;
 
     public void Start()
     {
@@ -16,6 +21,27 @@ public class LaunchControl : MonoBehaviour
             Debug.LogError("RigidBody2D not set.");
             enabled = false;
         }
+
+        // Hardcoding
+        EvilThrusts = new List<Vector2>();
+        EvilThrusts.Add(new Vector2(0.2f, 56f));
+        EvilThrusts.Add(new Vector2(0, 42.3f));
+        EvilThrusts.Add(new Vector2(0.4f, 59));
+        EvilThrusts.Add(new Vector2(-0.3f, 51.4f));
+        EvilThrusts.Add(new Vector2(-0.5f, 41.4f));
+        EvilThrusts.Add(new Vector2(0.3f, 55.3f));
+        EvilThrusts.Add(new Vector2(-0.2f, 35.1f));
+        EvilThrusts.Add(new Vector2(0, 59.8f));
+        EvilThrusts.Add(new Vector2(-0.2f, 68.4f));
+        
+        PrizeworthyThrusts = new List<Vector2>();
+        PrizeworthyThrusts.Add(new Vector2(0.3f, 67f));
+        PrizeworthyThrusts.Add(new Vector2(-0.1f, 65.4f));
+        PrizeworthyThrusts.Add(new Vector2(0.3f, 45.1f));
+        PrizeworthyThrusts.Add(new Vector2(0, 45.3f));
+        PrizeworthyThrusts.Add(new Vector2(-0.5f, 53.8f));
+        PrizeworthyThrusts.Add(new Vector2(-0.2f, 44.8f));
+        PrizeworthyThrusts.Add(new Vector2(-0.5f, 50.9f));
     }
 
     public void Update()
@@ -37,6 +63,29 @@ public class LaunchControl : MonoBehaviour
 
     private void LaunchBall()
     {
-        _rigidBody.AddForce(new Vector2(0, Random.Range(InitialThrust / 2, InitialThrust * 2)), ForceMode2D.Impulse);
+        HardcodedLaunching();
+    }
+
+    private void HardcodedLaunching()
+    {
+        if (LevelScript.Instance.NumberOfLives != 1)
+        {
+            var vector = EvilThrusts[Random.Range(0, EvilThrusts.Count - 1)];
+            Debug.Log("Evil " + vector);
+            _rigidBody.AddForce(vector, ForceMode2D.Impulse);
+        }
+        else
+        {
+            var vector = PrizeworthyThrusts[Random.Range(0, PrizeworthyThrusts.Count - 1)];
+            _rigidBody.AddForce(vector, ForceMode2D.Impulse);
+            Debug.Log("Good " + vector);
+        }
+    }
+
+    private void OrdinaryLaunching(out float thrustY, out float thrustX)
+    {
+        thrustY = Random.RandomRange(InitialThrust, InitialThrust * 2);
+        thrustX = Random.Range(-0.5f, 0.5f);
+        _rigidBody.AddForce(new Vector2(thrustX, thrustY), ForceMode2D.Impulse);
     }
 }
--- Assets/Code/LevelScript.cs
+++ Assets/Code/LevelScript.cs
@@ -6,16 +6,38 @@ using System.Linq;
 public class LevelScript : MonoBehaviour
 {
     public GameObject Player;
+    public GameObject DeathEffect;
+    public GameObject WinEffect;
     public float SpawnAboveValue = 10f;
+    public static LevelScript Instance;
+    public int MaxNumberOfLives = 3;
+
+    public float NumberOfLives
+    {
+        get
+        {
+            return _currentNumberOfLives;
+        }
+    }
 
     private GameObject _player;
     private List<ShrinkStars> _stars;
+    private int _currentNumberOfLives;
+    private bool _hasWon = false;
 
     public void Start()
     {
         InstantiatePlayer();
         _stars = new List<ShrinkStars>();
         _stars = FindObjectsOfType<ShrinkStars>().ToList<ShrinkStars>();
+        Instance = this;
+        _currentNumberOfLives = MaxNumberOfLives;
+
+        if (Player == null)
+        {
+            Debug.LogError("Assign Player to LevelScript!");
+            Application.Quit();
+        }
     }
 
     public void Update()
@@ -23,19 +45,69 @@ public class LevelScript : MonoBehaviour
         HandleInput();
     }
 
+    public void KillPlayer()
+    {
+        DestroyPlayer();
+        _currentNumberOfLives--;
+        if (_currentNumberOfLives == 0)
+            StartCoroutine(RestartGame());
+        else
+            InstantiatePlayer();
+    }
+
+    public void Win()
+    {
+        _hasWon = true;
+        DestroyPlayer();
+        StartCoroutine(RestartGame());
+    }
+
     private void HandleInput()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            Destroy(_player);
-            InstantiatePlayer();
-            RespawnStars();
+            DestroyPlayer();
+            _currentNumberOfLives = 0;
+            StartCoroutine(RestartGame());
         }
 
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            KillPlayer();
+        }
+    }
+
+    private void DestroyPlayer()
+    {
+        if (_player == null)
+            return;
+
+        if (!_hasWon)
+            if (DeathEffect != null)
+                Instantiate(DeathEffect, _player.transform.position, transform.rotation);
+
+        if (_hasWon)
+            if (WinEffect != null)
+                Instantiate(WinEffect, _player.transform.position, transform.rotation);
+
+        Destroy(_player);
+    }
+
+    private IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(1.5f);
+        _currentNumberOfLives = MaxNumberOfLives;
+        RespawnStars();
+        if (_player != null)
+            DestroyPlayer();
+        InstantiatePlayer();
+        _hasWon = false;
+        yield break;
     }
 
     private void InstantiatePlayer()
@@ -46,7 +118,7 @@ public class LevelScript : MonoBehaviour
 
     private void RespawnStars()
     {
-        foreach(ShrinkStars star in _stars)
+        foreach (ShrinkStars star in _stars)
         {
             star.Respawn();
         }
